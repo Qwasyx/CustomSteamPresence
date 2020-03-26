97b65efffb49c48cbb8b3a8924a6cda523e5916a
Qwasyx <qwasyx3000@gmail.com>
2020-03-27 00:59:56 +0100

    removed unused files and allow for comments in the settings

--- CustomSteamPresence/TextManager.cs
+++ CustomSteamPresence/TextManager.cs
@@ -10,7 +10,9 @@ namespace CustomSteamPresence
         private Random rand = new Random();
 
         private const string DEFAULT_CONFIG =
-@"Beating some levels [Hard]
+@"# One line per message
+# Everything after and including '#' is ignored, empty lines are removed
+Beating some levels [Hard]
 Punching wood!
 Murdering bloqs
 Finally I can beat Expert maps";
@@ -25,6 +27,18 @@ Finally I can beat Expert maps";
                 possibleStatuses = DEFAULT_CONFIG.Split('\n');
             }
             else possibleStatuses = File.ReadAllLines(fullpath);
+
+            //strip comments and empty lines
+            int actualStatuses = 0;
+            for(int i = 0; i < possibleStatuses.Length; ++i)
+            {
+                string text = possibleStatuses[i];
+                int index = text.IndexOf('#');
+                if (index >= 0) text = text.Substring(0, index);
+                if (text.Length > 0) possibleStatuses[actualStatuses++] = text;
+            }
+            Array.Resize(ref possibleStatuses, actualStatuses);
+
             Plugin.logger.Info("Read " + possibleStatuses.Length + " custom steam presence texts");
         }
 
